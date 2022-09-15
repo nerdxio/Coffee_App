fd8726a29823e0b719640dca37ba3243297ce500
Hassan Refaat <hassan.refaat.dev@gmail.com>
2022-09-15 04:18:31 +0200

    Enhancement

--- app/src/main/java/com/example/coffeeapp/ui/fargment/SignUpFragment.kt
+++ app/src/main/java/com/example/coffeeapp/ui/fargment/SignUpFragment.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.launch
 class SignUpFragment : Fragment() {
     lateinit var binding: FragmentSignUpBinding
     private val viewModel: SingUpViewModel by viewModels()
-
+    lateinit var dialog: Dialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,16 +36,45 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        dialog = Dialog(this.requireContext())
+        dialog.setContentView(R.layout.custom_dialog_for_sing_up)
+        val inset = InsetDrawable(
+            AppCompatResources.getDrawable(
+                this.requireContext(),
+                R.drawable.custom_dialog_background
+            ), 100
+        )
+        dialog.getWindow()?.setBackgroundDrawable(
+            inset
+        )
+        dialog.getWindow()
+            ?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.setCancelable(false) //Optional
+        dialog.getWindow()?.getAttributes()?.windowAnimations =
+            R.style.DialogAnimation //Setting the animations to dialog
+
+        val Okay: Button = dialog.findViewById(R.id.btn_okay)
+        val Cancel: Button = dialog.findViewById(R.id.btn_cancel)
+
+        Cancel.setOnClickListener {
+            dialog.dismiss()
+        }
+
+
         binding.btnSingUp.setOnClickListener {
             val name = binding.tvName.text.toString()
             val email = binding.tvEmail.text.toString()
             val password = binding.tvPassword.text.toString()
             viewModel.register(User(name, email, password))
-
+            dialog.show()
         }
-        viewModel.registerLiveData.observe(viewLifecycleOwner){ navigate ->
-            if(navigate) {
-                findNavController().navigate(R.id.action_signUpFragment_to_loginFragment2)
+        viewModel.registerLiveData.observe(viewLifecycleOwner) { navigate ->
+            if (navigate) {
+                Okay.setOnClickListener {
+                    dialog.dismiss()
+                    findNavController().navigate(R.id.action_signUpFragment_to_loginFragment2)
+                }
+
             }
         }
 
